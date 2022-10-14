75cab37c4d7e178a4dc9458eb66ae1f2b4807d2d
Akinwalee <obatulafuad@gmail.com>
2022-10-13 21:34:14 -0700

    Completed all tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+	puts("Programmin is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-sixe.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main(void){
+ 	char myCharacter;
+	int integer;
+	long int longInt;
+	long long int longLongInt;
+	float myFloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
+	printf("Size of a integer: %lu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(longInt));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongInt));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
+
+	return(0);
+ }
