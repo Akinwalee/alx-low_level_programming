33bea0d0fba149e8cc6ce132309aca766dd70f94
Akinwalee <obatulafuad@gmail.com>
2023-03-16 04:49:13 -0700

    Update files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,22 +2,22 @@
 /**
  * main - Prints type sizes
  *
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
- 	char myCharacter;
+	char myCharacter;
 	int integer;
 	long int longInt;
-	long long int lLInt;
+	long long int LInt;
 
 	float myFloat;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(myCharacter));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longInt));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lLInt));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(LInt));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(myFloat));
 
 	return (0);
