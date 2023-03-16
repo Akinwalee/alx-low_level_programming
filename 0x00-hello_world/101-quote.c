09ef748831115339e104047772f9234a13fb6a3a
Akinwalee <obatulafuad@gmail.com>
2023-03-16 05:26:54 -0700

    Update files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-	const char *message= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
+	const char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, m, strlen(m));
 
 	return (90);
 }
