256088006ea375d759fc486208c016f14dd76a03
Akinwalee <obatulafuad@gmail.com>
2023-03-16 04:40:15 -0700

    Update files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@
 	float myFloat;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(myCharacter));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(integer));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(longInt));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longLongInt));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(myFloat));
