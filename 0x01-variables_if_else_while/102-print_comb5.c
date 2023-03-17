2a6fc7ac587213a01640ea0e8b2f11b660f78a56
Akinwalee <obatulafuad@gmail.com>
2023-03-17 09:42:35 -0700

    Update files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,23 @@
 
 int main(void)
 {
-	int a;
+	int a, b;
 
-	for (a = 0; a <=99; a++)
+	for (a = 0; a <99; a++)
 	{
-		putchar((a / 10) + '0');
-		putchar((a % 10) + '0');
-		if (a == 99)
-			continue;
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
 		
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
