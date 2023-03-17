db9de6d3d56a8ec0ea4b2c76376d46b14f1af015
Akinwalee <obatulafuad@gmail.com>
2023-03-17 07:11:25 -0700

    Update files

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,18 +11,15 @@ int main(void)
 	int a;
 
 	for (a = 0; a <10; a++)
-	{	
-		if (a == 9)
+	{
+		putchar(a + '0');
+		if (a<9)
 		{
-			putchar(a + '0');
-		}
-		else
-		{
-			putchar(a + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
