2a53918fffc635fefc6d8cbc7b06293e668c8736
Akinwalee <obatulafuad@gmail.com>
2023-03-21 08:12:31 -0700

    Update files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,19 @@
 /**
  * _islower - Checks if a charcacter is in lower case and prints 1 or 0 if true or false
  *
+ *@c - variable for alphabet to be checked
+ *
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
 	if (c <= 65 && c <= 91)
+	{
 		return 0;
+	}
 	else
+	{
 		return 1;
+	}
 }
