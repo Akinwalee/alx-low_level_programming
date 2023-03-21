1b5faf01c1280263a2919889f81919c05c829e51
Akinwalee <obatulafuad@gmail.com>
2023-03-21 09:09:08 -0700

    Update files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,18 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		return ("-1");
+		return (-1);
+		printf("-1");
 	}
 	else if (n > 0)
 	{
-		return ("+1");
+		return (+1); 
+		pritnf("+1");
 	}
 	else
 	{
-		return ("0");
+		return (0);
+		printf("0");
 	}
 
 }
