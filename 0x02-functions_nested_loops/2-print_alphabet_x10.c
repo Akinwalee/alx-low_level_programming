c9a7e7e5af080153946a13e24ea9d75f55d93e33
Akinwalee <obatulafuad@gmail.com>
2023-03-21 07:30:44 -0700

    Update files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
  * print_alphabet_x10 - Prints all alphabets in lower case 10 times
  *
  * Return: Always 0 (Success)
  */
-
-void print_alphabet_x10(void);
-
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
-}
 
 void print_alphabet_x10(void)
 {
@@ -31,5 +22,4 @@ void print_alphabet_x10(void)
 
 		i++;
 	}
-	return;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,4 @@
 
 void print_alphabet(void);
 
-/*int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}*/
+void print_alphabet_x10(void);
