427c521bd9d23c98a194661bbfb1cce20bc3e00d
Akinwalee <obatulafuad@gmail.com>
2023-03-22 00:40:47 -0700

    Update files

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
  * add - Adds up two integers and return the result
  *
  * @a: Variable for the first integer
- * @b: Variable for the second integre
+ * @b: Variable for the second integer
  *
- * Return: The summ of integres a and b
+ * Return: The summ of integers a and b
  */
 
 int add(int a, int b)
