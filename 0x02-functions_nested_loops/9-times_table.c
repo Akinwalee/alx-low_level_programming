29d32fe408e678b5b3cf4eb43590eff8a385c5b6
Akinwalee <obatulafuad@gmail.com>
2023-03-21 21:33:28 -0700

    Update files

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the multiplication table from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d", (i * j));
+			if (j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+
+	return;
+}
