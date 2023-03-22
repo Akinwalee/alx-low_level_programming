b17250565a4d0cbd481716a91ce8cca8ae6e8136
Akinwalee <obatulafuad@gmail.com>
2023-03-22 00:22:54 -0700

    Update files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,19 +8,18 @@
 
 void times_table(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int prod = j * i;
+			int product = j * i;
 
 			if (j == 0)
 			{
 				_putchar('0');
-			} else if (prod <= 9)
+			} else if (product <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -30,8 +29,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(prod / 10 + '0');
-				_putchar(prod % 10 + '0');
+			//	_putchar(prod / 10 + '0');
+				_putchar(prod + '0');
 			}
 		}
 		_putchar('\n');
