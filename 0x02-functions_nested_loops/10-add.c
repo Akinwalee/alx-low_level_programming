c17d175361539162628905d3b72e29ec9c456e79
Akinwalee <obatulafuad@gmail.com>
2023-03-22 00:35:52 -0700

    Update files

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,1 @@
-#include "main.h"
 
-/**
- * add - Adds up 2 numbers
- *
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of both integers
- */
-
-int add(int a, int b)
-{
-	return (a + b);
-}
