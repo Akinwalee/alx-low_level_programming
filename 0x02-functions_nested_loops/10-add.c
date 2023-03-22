5aba0843ceb08bbe91d12eade74e39c5de132b98
Akinwalee <obatulafuad@gmail.com>
2023-03-22 00:38:56 -0700

    Update files

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * add - Adds up two integers and return the result
+ *
+ * @a: Variable for the first integer
+ * @b: Variable for the second integre
+ *
+ * Return: The summ of integres a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
