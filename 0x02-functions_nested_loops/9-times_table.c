47c26324cf7be488dff849a3a096c7dfe31c9034
Akinwalee <obatulafuad@gmail.com>
2023-03-22 00:23:48 -0700

    Update files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,13 +24,13 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(prod + '0');
+				_putchar(product + '0');
 			} else
 			{
 				_putchar(',');
 				_putchar(' ');
 			//	_putchar(prod / 10 + '0');
-				_putchar(prod + '0');
+				_putchar(product + '0');
 			}
 		}
 		_putchar('\n');
