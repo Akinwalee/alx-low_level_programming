306fe7bad20a957f438b63f452d3077e274d6492
Akinwalee <obatulafuad@gmail.com>
2023-03-27 15:50:40 -0700

    Update files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ *
+ * @s: the string to be printed
+ *
+ * Return: void.
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	char new_string[];
+
+	for (i = lenght - 1; i >= 0; i--)
+	{
+		new_string = s[i];
+	}
+	puts(new_string);
+}
