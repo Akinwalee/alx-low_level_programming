4db5b810297bf7484954f4b1734b69e68ce54f3d
Akinwalee <obatulafuad@gmail.com>
2023-03-27 21:51:03 -0700

    Update files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,17 @@ void reset_to_98();
 void swap_int();
 
 int _strlen();
+
+void print_rev();
+
+void rev_string();
+
+void puts2();
+
+void puts_half();
+
+void print_array();
+
+char *_strcpy();
+
+int _putchar();
