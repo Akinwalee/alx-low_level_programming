9c5532e4ea47782741ed6316f5498482c811e60a
Akinwalee <obatulafuad@gmail.com>
2023-03-29 13:42:01 -0700

    Update files

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char *_strcat();
+
+char *_strncat();
+
+char *_strncpy();
+
+int _strcmp();
+
+void reverse_array();
+
+char *string_toupper();
+
+char *cap_string();
+
+char *leet();
+
+char *rot13();
+
+void print_number();
+
+char *infinite_add();
+
+void print_buffer();
