e80b18fb3f37fa15e4ec88db09d42d75b7a5bcb0
Akinwalee <obatulafuad@gmail.com>
2023-03-31 07:32:34 -0700

    Update files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ *
+ * @s1: First string to compare
+ * @s2: Second string to compare with.
+ *
+ * Return: A value indicating the relationship between the 2 strings.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
