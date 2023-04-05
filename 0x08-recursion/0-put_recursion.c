95876544625d6b6490a691d149664d915b362cef
Akinwalee <obatulafuad@gmail.com>
2023-04-05 09:55:49 -0700

    Update files

---
+++ 0x08-recursion/0-put_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ *
+ * @s: The string to print.
+ *
+ * Return: Void.
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int _putchar();
+
+void _puts_recursion();
+
+void _print_rev_recursion();
+
+int _strlen_recursion();
+
+int factorial();
+
+int _pow_recursion();
+
+int _sqrt_recursion();
+
+int is_prime_number();
+
+int is_palindrome();
+
+int wildcmp();
