2bb67caff77aa602343f0ffbf860a290afde2938
Akinwalee <obatulafuad@gmail.com>
2023-04-12 14:04:41 -0700

    Update files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * whatsmyname - Prints the name of the function
+ *
+ * @argc: argc
+ *
+ * Return: void
+ */
+:x
+void whatsmyname(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+}
