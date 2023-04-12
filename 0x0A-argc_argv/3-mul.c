bc5fef29d4b011b80c3b31eb89aee26a91c71267
Akinwalee <obatulafuad@gmail.com>
2023-04-12 15:55:56 -0700

    Update files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - Multiplies 2 numbers passed as an argument to the program
+ *
+ * @argc: Argc
+ * @argv: Argv
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+
+		printf("%d\n", num1 * num2);
+		
+		return (0);
+	}
+	
+	printf("Error\n");
+
+	return (0);
+}
