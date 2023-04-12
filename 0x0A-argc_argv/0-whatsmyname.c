36497d4463ff803e61e3ccf23d9f8425a77e601c
Akinwalee <obatulafuad@gmail.com>
2023-04-12 14:54:23 -0700

    Update files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: void
  */
 
-void myname(int __attribute__ ((unused)) argc, char *argv[])
+void myname(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 }
