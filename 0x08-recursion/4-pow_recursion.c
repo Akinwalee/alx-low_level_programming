204fe45f2cb963df54b9e0974400dea45ce55a05
Akinwalee <obatulafuad@gmail.com>
2023-05-01 07:01:11 -0700

    Update files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,11 @@
  * Return: The value of x raised to y.
  */
 
-intt _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
