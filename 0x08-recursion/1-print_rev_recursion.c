98f1ae693e63f676bfa6cb2afecaae03a1eb0256
Akinwalee <obatulafuad@gmail.com>
2023-05-01 06:23:58 -0700

    Update files

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,26 +6,15 @@
  * helper - Helper function for the main recursion.
  *
  * @s: The string to reverse.
- * @i: String index variable.
- * @j: String end index variable.
  *
  * Return: Void.
  */
 
-void helper(char *s, int i, int j)
-{
-	char temp;
-
-	temp = s[i];
-	s[i] = s[j];
-	s[j] = temp;
-	helper(s, i + 1, j - 1);
-}
-
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s);
-	char *str = *s;
-	helper(s, 0, len - 1);
-	printf("%s", s);
+	if (*s != NULL)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
