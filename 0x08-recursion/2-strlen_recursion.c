03bbe688284340b5838e640179a1d8720bf3e564
Akinwalee <obatulafuad@gmail.com>
2023-05-01 06:34:20 -0700

    Update files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,3 +4,19 @@
  * _strlen_recursion - Return the length of a string
  *
  * @s: The string whose length will be returned
+ *
+ * Return: The legnth of the string stored in variable s.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length = length + 1;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
+}
