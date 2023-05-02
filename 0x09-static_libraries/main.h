7497ef567656058369a7f265041797ea9af44b87
Akinwalee <obatulafuad@gmail.com>
2023-05-02 03:12:36 -0700

    Update files

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -8,7 +8,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-char *_strcpy(char *dest, char *src);
+char *_strcpy();
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
