397485f417cccf41faa0400e18c6a3eebfa63f36
Akinwalee <obatulafuad@gmail.com>
2023-05-02 10:29:24 -0700

    Update files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name
+ *
+ * @name: name to print
+ * @f: function to use
+ *
+ * Return: Void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
