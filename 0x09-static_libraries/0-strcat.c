1aa8bb8ec62cb7e921792265b5c51530a9b0d60a
Akinwalee <obatulafuad@gmail.com>
2023-05-02 02:48:08 -0700

    Update files

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strcat - Contenates two strings
+ *
+ * @dest: First string
+ * @src: Second string
+ *
+ * Return: A pointer to the resulting concatenated string in variable dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
