2d6a675a5e8025f8e0a5ce270c8c841e286e4ded
Akinwalee <obatulafuad@gmail.com>
2023-05-02 10:45:35 -0700

    Update files

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - gets the right func
+ *
+ * @s: function s
+ *
+ * Return: int value
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].f != NULL)
+	{
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+
+	printf("Error\n");
+	exit(99);
+}
