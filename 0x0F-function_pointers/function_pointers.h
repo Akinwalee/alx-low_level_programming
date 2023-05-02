3b49bec31958e6df9ef3e19e4be8630887e7559a
Akinwalee <obatulafuad@gmail.com>
2023-05-02 10:34:33 -0700

    Update files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
