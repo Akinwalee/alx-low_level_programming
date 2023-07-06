a843c187f4bad17e95e2d8be42e4d16598c97a38
Obatula Fuad Adebari <74328942+Akinwalee@users.noreply.github.com>
2023-07-06 20:05:00 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,13 @@ char *string_toupper(char *a)
 {
 	int i = 0;
 
-	for (i = 0; i != '\0'; i++)
+	while (a[i])
 	{
 		if (a[i] >= 'a' && a[i] <= 'z')
-			a[i] = a[i] -32
+			a[i] -= 32;
+
+		i++;
 	}
+
+	return (a);
 }
