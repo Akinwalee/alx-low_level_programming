b16980943dd01309b8f0a174f83ad79e21e1184b
Akinwalee <obatulafuad@gmail.com>
2023-08-02 05:44:50 -0700

    Rewrite recursive square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * helper - Hlper function for square root
+ *
+ * @n: The number whose square root is to be found
+ * @guess: the babylonian guess
+ *
+ * return: The square root of n
+ */
+
+int helper(int n, int guess)
+{
+	int i = (guess + n / guess) / 2;
+
+	if (i == guess)
+	{
+		return (guess);
+	}
+	else
+	{
+		return (helper(n, i));
+	}
+}
+
+/**
  * _sqrt_recursion - Returns the natural square root of a number
  *
  * @n: The number whose square root is to be returned.
@@ -10,13 +33,10 @@
 
 int _sqrt_recursion(int n)
 {
-	int i;
-
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
 
-	i = _sqrt_recursion(n - 1);
-	return (i + (n / i) / 2);
+	return (helper(n, n / 2));
 }
