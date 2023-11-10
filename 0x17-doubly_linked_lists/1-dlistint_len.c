cc54e1129c7b9fd8d636a1c638598e66e0420eb2
Akinwalee <obatulafuad@gmail.com>
2023-11-10 03:44:11 +0000

    Updated files

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Computes the length of a doubly linked list
+ *
+ * @head: Pointer to the head of the doubly linked list
+ *
+ * Return: Lenght of the soubly linked list.
+ */
+
+size_t dlistint_len(const dlistint_t *head)
+{
+	int count = 0;
+
+	if (head == NULL)
+	{
+		return (1);
+	}
+
+	while (head)
+	{
+		count++;
+		head = head->next;
+	}
+
+	return (count);
+}
