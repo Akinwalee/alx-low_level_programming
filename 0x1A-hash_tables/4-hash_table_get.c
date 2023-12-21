844d139f51bd6a5a6145ead41d327d4200e344dc
Akinwalee <obatulafuad@gmail.com>
2023-12-21 09:44:07 +0000

    Updated files

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function that retrieves the value associated to a key
+ *
+ * @ht: The hash table
+ * @key: The key whose value is to be retrieved
+ *
+ * Return: Value associated with the key (Success) or NULL (Key is not found)
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+	hash_node_t *current = NULL;
+
+	if (ht == NULL)
+		return (NULL);
+
+	index = key_index(key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	current = ht->array[index];
+	return (current->value);
+}
